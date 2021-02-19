a294f4a2af9cd76da38bda8495f1a3f4ad56b3d0
galbe <galbe@192.168.1.70>
2021-02-19 11:07:53 -0600

    Mi trabajo

---
+++ src/git66137/Git66137.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git66137;
+
+public class Git66137 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String nombre= "Katia Guadalupe Calan Chi ";
+        String matricula= "66137";
+        System.out.println(" SOY " + nombre + " Y MI MATRICULA ES " + matricula);
+        // ACABO DE CREAR MI REPOSITORIO LOCAL EN TEAM
+    }
+    
+}
